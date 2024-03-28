996035c2b6c05e51a0c494334722b005227daf47
gzr <2900248206@qq.com>
2024-03-28 13:14:11 +0800

    优化个人主页

--- expert_finding_system_frontend/src/pages/Introduction/achievementExhibition.vue
+++ expert_finding_system_frontend/src/pages/Introduction/achievementExhibition.vue
@@ -1,60 +1,70 @@
 <template>
-  <el-card class="info-card">
-  <el-card>
-    <el-icon><location /></el-icon>论文：
-  </el-card>
-    <ul>
-      <li v-for="(paper, index) in papers" :key="index">
-        {{ paper.authors }}. {{ paper.title }}. {{ paper.journal }}, {{ paper.year }}
-      </li>
-    </ul>
-  </el-card>
+  <div>
+    <el-collapse v-model="activeType" accordion>
+      <el-collapse-item v-for="(type, index) in types" :title="type" :name="type" :key="index">
+        <ul v-if="type === '论文'" class="paper-list">
+          <li v-for="(paper, index) in papers" :key="index">
+            <span class="paper-info">{{ paper.authors }}. {{ paper.title }}. {{ paper.journal }}, {{ paper.year }}</span>
+          </li>
+        </ul>
 
-  <el-card class="info-card">
-  <el-card>
-    <el-icon><location /></el-icon>著作：
-  </el-card>
-    <ul>
-      <li v-for="(book, index) in books" :key="index">
-        {{ book.authors }}. {{ book.title }}. {{ book.publisher }}, {{ book.year }}
-      </li>
-    </ul>
-  </el-card>
+        <ul v-if="type === '著作'" class="book-list">
+          <li v-for="(book, index) in books" :key="index">
+            <span class="book-info">{{ book.authors }}. {{ book.title }}. {{ book.publisher }}, {{ book.year }}</span>
+          </li>
+        </ul>
 
-  <el-card class="info-card">
-    <el-card>
-      <el-icon><location /></el-icon>项目：
-    </el-card>  
-    <ul>
-      <li v-for="(project, index) in projects" :key="index">
-        {{ project.title }} - {{ project.duration }}. 主持人: {{ project.leader }}
-      </li>
-    </ul>
-  </el-card>
+        <ul v-if="type === '项目'" class="project-list">
+          <li v-for="(project, index) in projects" :key="index">
+            <span class="project-info">{{ project.title }} - {{ project.duration }}. 主持人: {{ project.leader }}</span>
+          </li>
+        </ul>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+
+const activeType = ref('论文');
+
+const types = ['论文', '著作', '项目'];
 
 const papers = reactive([
   { authors: 'Ling Huang, Ye Zhu, Yuefang Gao*, Tuo Liu, Chao Chang, Caixing Liu, Yong Tang*, Chang-Dong Wang', title: 'Hybrid-Order Anomaly Detection on Attributed Networks', journal: 'IEEE Transactions on Knowledge and Data Engineering', year: 'Dec. 2023' },
   { authors: 'Binjie09', title: 'AI Development with Vue', journal: 'Vue Publications', year: '2022' },
-  // 其他论文数据...
 ]);
 
 const books = reactive([
   { authors: 'Yong Tang, Qiaohong Zu, José G. Rodríguez García', title: 'Human Centered Computing (HCC 2018)', publisher: 'Springer', year: '2019.03' },
   { authors: 'Binjie09', title: 'Mastering Vue 3', publisher: 'Vue Press', year: '2021' },
-  // 其他著作数据...
 ]);
 
 const projects = reactive([
   { title: '基于教育大数据的精准教学评估与学习行为预测方法研究', duration: '2019.01-2022.12', leader: '汤庸' },
   { title: '智能交通系统优化设计', duration: '2020.05-2023.08', leader: '张三' },
-  // 其他项目数据...
 ]);
 </script>
 
-<style scoped>
 
+<style scoped>
+  .paper-list {
+  font-family: 'Arial', sans-serif; /* 论文列表字体 */
+}
+
+.book-list {
+  font-family: 'Georgia', serif; /* 著作列表字体 */
+}
+
+.project-list {
+  font-family: 'Verdana', sans-serif; /* 项目列表字体 */
+}
+
+.paper-info,
+.book-info,
+.project-info {
+  font-size: 16px; /* 设置内容字体大小 */
+  margin-bottom: 8px; /* 设置内容间距 */
+}
 </style>
--- expert_finding_system_frontend/src/pages/Introduction/personalInfo.vue
+++ expert_finding_system_frontend/src/pages/Introduction/personalInfo.vue
@@ -1,20 +1,31 @@
 <template>
-   <el-card class="teacher-card">
-    <div>
-      <span>张三，苏州大学</span>
-    </div>
-    <div>
-      <p>创始人、学者网</p>
-      <p>二级教授，国务院政府特殊津贴专家</p>
-      <p>教育部新世纪优秀人才，广东特支计划教学名师</p>
-      <p>研究方向：数据智能与社交网络</p>
-      <p>学历：武汉大学学士和硕士，中国科学技术大学博士</p>
-      <p>荣誉奖项：丁颖科技奖、宝钢教育奖等多个奖项</p>
+  <el-card class="teacher-card">
+    <el-descriptions :bordered="true" column="1" border>
+      <el-descriptions-item label="姓名">{{ teacher.name }}</el-descriptions-item>
+      <el-descriptions-item label="所属学校">{{ teacher.university }}</el-descriptions-item>
+      <el-descriptions-item label="职称">{{ teacher.title }}</el-descriptions-item>
+      <el-descriptions-item label="研究方向">{{ teacher.research }}</el-descriptions-item>
+      <el-descriptions-item label="学历">{{ teacher.education }}</el-descriptions-item>
+      <el-descriptions-item label="荣誉奖项">{{ teacher.awards }}</el-descriptions-item>
+    </el-descriptions>
+    <div class="tags">
+      <el-tag type="success">{{ teacher.tags[0] }}</el-tag>
+      <el-tag type="info">{{ teacher.tags[1] }}</el-tag>
+      <el-tag type="warning">{{ teacher.tags[2] }}</el-tag>
     </div>
   </el-card>
 </template>
-
-<script lang="ts">
+<script lang="ts" setup>
+import { reactive } from "vue";
+  const teacher = reactive({
+      name: '张三',
+      university: '苏州大学',
+      title: '创始人、学者网\n二级教授，国务院政府特殊津贴专家\n教育部新世纪优秀人才，广东特支计划教学名师',
+      research: '数据智能与社交网络',
+      education: '武汉大学学士和硕士，中国科学技术大学博士',
+      awards: '丁颖科技奖、宝钢教育奖等多个奖项',
+      tags: ['计算机科学', '人工智能', '数据分析']
+    });
 
 </script>
 
